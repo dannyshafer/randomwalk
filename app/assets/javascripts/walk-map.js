bda53d49fb462c40e8182743e60c454a804d9251
Elliot Lee <github.public@intelliot.com>
2016-02-26 22:04:40 -0800

    Add map

--- app/assets/javascripts/walk-map.js
+++ app/assets/javascripts/walk-map.js
@@ -1,7 +1,3 @@
-<h1>Welcome to RandomWalk</h1>
-
-<div id='map'></div>
-<script>
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFuZG9td2FsayIsImEiOiJjaWw0Y3B5dzEwMjl1dGhseXcyOWh5NGJpIn0.ZKl7LVbPulKaAqQjbsMJfQ';
 var map = new mapboxgl.Map({
     container: 'map', // container id
@@ -9,4 +5,3 @@ var map = new mapboxgl.Map({
     center: [-74.50, 40], // starting position
     zoom: 9 // starting zoom
 });
-</script>
