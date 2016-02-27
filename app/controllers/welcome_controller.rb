cbedff3af82b72f1ae8b5ff41d7887e79caf99b7
Daniel Shafer <github@dannyshafer.com>
2016-02-27 11:36:16 -0800

    make route, view, and controller action for distance submission

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+	def get_directions
+		#api calls for getting directions goes here
+		puts "8============D~~~~~~~~~~"*1000
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get '/get_directions' => 'welcome#get_directions'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
