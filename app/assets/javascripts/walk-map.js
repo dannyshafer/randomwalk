813943fbc16d7db88e0f855a415a8769b8af5ba9
Elliot Lee <github.public@intelliot.com>
2016-02-27 11:57:34 -0800

    Add current location
    - Add button to "Get Current Location"
    - Create map after location is retrieved, centered on user's location
    - Add marker for the user's current location
    - Reduce map size to 80% width and 400px height
    - Add loading spinner
    - Add map styles
    - Remove redundant code from walk-map.js

--- app/assets/javascripts/walk-map.js
+++
@@ -1,7 +0,0 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoicmFuZG9td2FsayIsImEiOiJjaWw0Y3B5dzEwMjl1dGhseXcyOWh5NGJpIn0.ZKl7LVbPulKaAqQjbsMJfQ';
-var map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
-    center: [-74.50, 40], // starting position
-    zoom: 9 // starting zoom
-});
