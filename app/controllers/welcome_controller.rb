77fa122cd51f71e4db12e6e919a2bb5935d96543
Daniel Shafer <github@dannyshafer.com>
2016-02-27 23:39:01 -0800

    add get compass path to make route generation more dynamic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,8 @@ class WelcomeController < ApplicationController
 
 	def get_directions
 		#api calls for getting directions goes here
-		@destination_lon, @destination_lat = get_destination(params[:lon], params[:lat], params[:distance])
-		@directions = HTTParty.get("https://api.mapbox.com/v4/directions/mapbox.walking/#{params[:lon]},#{params[:lat]};#{@destination_lon},#{@destination_lat}.json?access_token=pk.eyJ1IjoicmFuZG9td2FsayIsImEiOiJjaWw0Y3B5dzEwMjl1dGhseXcyOWh5NGJpIn0.ZKl7LVbPulKaAqQjbsMJfQ")
+		@destination_lon, @destination_lat = get_destination(params[:lon], params[:lat], (params[:distance].to_f/2))
+		@directions = HTTParty.get("https://api.mapbox.com/v4/directions/mapbox.walking/#{params[:lon]},#{params[:lat]};#{@destination_lon},#{@destination_lat};#{params[:lon]},#{params[:lat]}.json?access_token=pk.eyJ1IjoicmFuZG9td2FsayIsImEiOiJjaWw0Y3B5dzEwMjl1dGhseXcyOWh5NGJpIn0.ZKl7LVbPulKaAqQjbsMJfQ")
 	end
 
 	private
@@ -52,5 +52,26 @@ class WelcomeController < ApplicationController
 			end
 		end
 		return lon, lat
+	end
+
+	def get_compass_path
+		#sets a compass path (n,w,s,origin) (s,w,n,origin)
+		# north = 1, south = 2, east = 3, west = 4
+		first_direction = [1,2,3,4].sample
+		case first_direction 
+		when 1
+			second_direction = [3,4].sample
+			third_direction = 2
+		when 2
+			second_direction = [3,4].sample
+			third_direction = 1
+		when 3
+			second_direction = [1,2].sample
+			third_direction = 4
+		when 4
+			second_direction = [1,2].sample
+			third_direction = 3
+		end
+		return [first_direction, second_direction, third_direction]
 	end
 end
