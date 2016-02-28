8d8391cf4eaf6fa2ab1e7ffe19ae321073fe2d1e
Elliot Lee <github.public@intelliot.com>
2016-02-27 23:04:12 -0800

    Add map with directions to get_directions page.
    - Automatically get user's current location on the index page.
    - Disable form submit button until location has loaded.
    - Set hidden form values to user's longitude and latitude.
    - Increase map height to 550px.
    - Use Mapbox GL Directions
      - https://github.com/mapbox/mapbox-gl-directions
      - Mapbox GL Directions is a client-side plugin.
      - Remove unnecessary server-side API call.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,8 @@
 class WelcomeController < ApplicationController
 
 	def get_directions
-		#api calls for getting directions goes here
 		@destination_lon, @destination_lat = get_destination(params[:lon], params[:lat], params[:distance])
-		@directions = HTTParty.get("https://api.mapbox.com/v4/directions/mapbox.walking/#{params[:lon]},#{params[:lat]};#{@destination_lon},#{@destination_lat}.json?access_token=pk.eyJ1IjoicmFuZG9td2FsayIsImEiOiJjaWw0Y3B5dzEwMjl1dGhseXcyOWh5NGJpIn0.ZKl7LVbPulKaAqQjbsMJfQ")
+		# @directions = HTTParty.get("https://api.mapbox.com/v4/directions/mapbox.walking/#{params[:lon]},#{params[:lat]};#{@destination_lon},#{@destination_lat}.json?access_token=pk.eyJ1IjoicmFuZG9td2FsayIsImEiOiJjaWw0Y3B5dzEwMjl1dGhseXcyOWh5NGJpIn0.ZKl7LVbPulKaAqQjbsMJfQ")
 	end
 
 	private
